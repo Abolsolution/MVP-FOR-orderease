08ab51468f4064af6b484c6d72da1b418d1d34d8
mercy <mihretyirga7@gmail.com>
2025-05-07 14:24:17 +0300

    Add reset password page

---
+++ reset.js
@@ -0,0 +1,33 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('resetForm');
+    const emailInput = document.getElementById('emailInput');
+    const messageBox = document.getElementById('message');
+  
+    
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      
+      const email = emailInput.value.trim();
+
+      messageBox.textContent = '';
+      messageBox.className = 'sucess-message';
+  
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+      if (emailPattern.test(email)) {
+        messageBox.textContent = `✅ A reset link has been sent to ${email}`;
+        messageBox.style.color = '#006400';
+  
+        setTimeout(() => {
+            form.reset();
+          }, 2000);
+  
+        
+      } else {
+        messageBox.textContent = '❌ Please enter a valid email address.';
+        messageBox.style.color = '#006400';
+      }
+    });
+  });
+  
